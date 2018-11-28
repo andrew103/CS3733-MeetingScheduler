5b4021014df2af9c5568eb88a946a4ba852fd167
Andrew Euredjian <andrew_103@rocketmail.com>
2018-11-28 15:02:23 -0500

    Add database access objects

---
+++ src/main/java/db/SchedulerDAO.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.Day;
+import entity.Meeting;
+import entity.Schedule;
+import entity.Timeslot;
+import entity.Scheduler;
+
+public class SchedulerDAO {
+
+	java.sql.Connection conn;
+
+    public SchedulerDAO() {
+    	try  {
+    		conn = DatabaseUtils.connect();
+    	} catch (Exception e) {
+    		conn = null;
+    	}
+    }
+
+}
