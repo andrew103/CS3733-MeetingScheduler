315deba46c19ebedf715dd2e8f0a2d49e42e1cd7
Ryan <Ryan@autoreg-708867.wifi.wpi.edu>
2018-11-27 21:03:07 -0500

    updated Schedule and Scheduler. sort of

--- src/main/java/entity/Scheduler.java
+++ src/main/java/entity/Scheduler.java
@@ -1,30 +1,74 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Scheduler 
 {
-	private ArrayList<Schedule> schedules;
+	public ArrayList<Schedule> schedules;
 	
 	public Scheduler()
 	{
 		
 	}
 	
-	public void createSchedule(Schedule s)
+	public void createSchedule(Schedule schedule)
 	{
-		schedules.add(s);
+		System.out.println(""+schedule.scheduleName);
+		schedules.add(schedule);
 	}
 	
 	public ArrayList<Schedule> requestOldSchedules(int days)
 	{
 		ArrayList<Schedule> sched = new ArrayList<Schedule>();
+		Date current = new Date();
 		for (Schedule schedule : schedules)
 		{
 			//If schedule is more than 'days' days old, return it
-			//if (schedule.getStartDate())
-			schedules.add(schedule);
+			if (current.getDate() - schedule.getStartDate().getDate() <= days)
+			{
+				sched.add(schedule);
+			}
 		}
 		return sched;
+	}
+	
+	public void deleteOldSchedules(int days)
+	{
+		Date current = new Date();
+		for (Schedule schedule : schedules)
+		{
+			if (current.getDate() - schedule.getStartDate().getDate() <= days)
+			{
+				schedules.remove(schedule);
+			}
+		}
+	}
+	
+	public ArrayList<Schedule> requestNewSchedules(int hours) 
+	{
+		ArrayList<Schedule> sched = new ArrayList<Schedule>();
+		Date current = new Date();
+		for (Schedule schedule : schedules)
+		{
+			if (current.getDate() - schedule.getStartDate().getDate() <= hours)
+			{
+				sched.add(schedule);
+			}
+		}
+		return sched;
+		
+	}
+	
+	public Schedule retrieveSchedule(String shareCode)
+	{
+		for (Schedule s : schedules)
+		{
+			if (s.getShareCode().equals(shareCode))
+			{
+				return s;
+			}
+		}
+		return null;
 	}
 }
