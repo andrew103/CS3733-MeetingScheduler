e8764bfc07547a21af969041a1137182d78b4269
Chenggu Wang <cwang6@wpi.edu>
2018-11-29 16:48:13 -0500

    lambda function works now

--- src/main/java/com/amazonaws/lambda/demo/CreateScheduleRequest.java
+++ src/main/java/com/amazonaws/lambda/demo/CreateScheduleRequest.java
@@ -10,18 +10,29 @@ class CreateScheduleRequest {
 	GregorianCalendar endDate;
 	int startTime;
 	int endTime;
+	String sd;
+	String ed;
 	
-	public CreateScheduleRequest(String name, int duration, GregorianCalendar sd, GregorianCalendar ed, int st, int et) {
+	public CreateScheduleRequest(String name, int duration, String sd, String ed, int st, int et) {
 		this.scheduleName = name;
 		this.meetingDuration = duration;
-		this.startDate = sd;
-		this.endDate = ed;
+		this.startDate = parseDate(sd);
+		this.endDate = parseDate(ed);
+		this.sd = sd;
+		this.ed = ed;
 		this.startTime = st;
 		this.endTime = et;
 	}
 	
+	public GregorianCalendar parseDate(String date) { ///take in date as "YYYY-MM-DD"
+		int year = Integer.parseInt(date.substring(0, 3));
+		int month = Integer.parseInt(date.substring(5, 6));
+		int day = Integer.parseInt(date.substring(8, 9));
+		return new GregorianCalendar(year, month, day);
+	}
+	
 	public String toString() {
-		return "Creating schedule " + scheduleName + " from " + startDate.getTime().toString() + " to " + endDate.getTime().toString() +
+		return "Creating schedule " + scheduleName + " from " + sd + " to " + ed +
 				"\n Meetings start at " + startTime + " ends at " + endTime + " for " + meetingDuration + " minutes each.";
 	}
 }
