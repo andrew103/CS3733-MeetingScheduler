22877edcdaf0fd9436c22c9f96a19691a1a7456e
akash <akashshaji321@gmail.com>
2018-12-06 23:05:10 -0500

    working on getSchedule

--- src/main/java/com/amazonaws/lambda/getSchedule/GetScheduleRequest.java
+++ src/main/java/com/amazonaws/lambda/getSchedule/GetScheduleRequest.java
@@ -1,5 +1,17 @@
 package com.amazonaws.lambda.getSchedule;
 
 public class GetScheduleRequest {
+	
+	String scheduleCode;
+	
+	public GetScheduleRequest(String scheduleCode)
+	{
+		this.scheduleCode = scheduleCode;
+	}
+	
+	public String toString()
+	{
+		return "Getting Schedule with code" + scheduleCode;
+	}
 
 }
--- src/main/java/com/amazonaws/lambda/getSchedule/GetScheduleResponse.java
+++ src/main/java/com/amazonaws/lambda/getSchedule/GetScheduleResponse.java
@@ -1,5 +1,24 @@
 package com.amazonaws.lambda.getSchedule;
 
 public class GetScheduleResponse {
+	
+	String scheduleCode;
+	String secretCode;
+	int httpCode; 
+	
+	public GetScheduleResponse(String scheduleCode, String secretCode, int hc) {
+		this.scheduleCode = scheduleCode;
+		this.secretCode = secretCode;
+		this.httpCode = hc;
+	}
+
+	public GetScheduleResponse(String message, int code) {
+		System.out.println(message);
+		this.httpCode = 200;
+	}
+	
+	public String toString() {
+		return "I'm trying to get" + scheduleCode + "here is the secret code" + secretCode;
+	}
 
 }
