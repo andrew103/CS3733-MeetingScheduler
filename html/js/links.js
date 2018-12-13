ec3fd2e0a331caefc63678fbd387fc8c4c262d06
akash <akashshaji321@gmail.com>
2018-12-12 20:49:59 -0500

    links

--- html/js/links.js
+++ html/js/links.js
@@ -1,7 +1,12 @@
-organizerWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/html/organizerPage.html"
 
-participantWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/html/participantPage.html"
 
-indexWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/html/index.html"
 
-sysAdminWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/html/sysAdminPage.html"
+folder = "html00001"
+
+organizerWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/" + folder + "/organizerPage.html"
+
+participantWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/" + folder + "/participantPage.html"
+
+indexWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/" + folder + "/index.html"
+
+sysAdminWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/" + folder + "/sysAdminPage.html"
