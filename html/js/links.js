b3d958047dd67d6ea6f3db2e437107ba368a2e9f
Chenggu Wang <cwang6@wpi.edu>
2018-12-13 13:31:22 -0500

    fixed link and added cache protection

--- html/js/links.js
+++ html/js/links.js
@@ -5,10 +5,18 @@ folder = "html"
 
 
 
-organizerWebsite = "file:///Users/apple/git/CS3733-MeetingScheduler/" + folder + "/organizerPage.html"
-
-participantWebsite = "file:///Users/apple/git/CS3733-MeetingScheduler/" + folder + "/participantPage.html"
+// indexWebsite = "file:///Users/apple/git/CS3733-MeetingScheduler/" + folder + "/index.html"
+//
+// sysAdminWebsite = "file:///Users/apple/git/CS3733-MeetingScheduler/" + folder + "/sysAdminPage.html"
+//
+// organizerWebsite = "file:///Users/apple/git/CS3733-MeetingScheduler/" + folder + "/organizerPage.html"
+//
+// participantWebsite = "file:///Users/apple/git/CS3733-MeetingScheduler/" + folder + "/participantPage.html"
 
 indexWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/" + folder + "/index.html"
 
 sysAdminWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/" + folder + "/sysAdminPage.html"
+
+organizerWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/" + folder + "/organizerPage.html"
+
+participantWebsite = "https://s3.us-east-2.amazonaws.com/scheduler-storage/" + folder + "/participantPage.html"
